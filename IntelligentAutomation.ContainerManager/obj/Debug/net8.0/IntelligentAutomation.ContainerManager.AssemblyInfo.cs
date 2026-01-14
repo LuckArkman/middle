a2fd115f7263e01072da7126b94f3456093c56ff
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-01-14 00:14:30 -0300

    UP

--- IntelligentAutomation.ContainerManager/obj/Debug/net8.0/IntelligentAutomation.ContainerManager.AssemblyInfo.cs
+++ IntelligentAutomation.ContainerManager/obj/Debug/net8.0/IntelligentAutomation.ContainerManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntelligentAutomation.ContainerManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a529feb1ecc8a1b7c00a29f0d3555aca27807090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58bb2e3081c88637ae7ef64fe5f1746b19f0c3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntelligentAutomation.ContainerManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntelligentAutomation.ContainerManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
